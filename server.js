14befaf207451b88349347926e94ca01fc0d3782
Ebrima saho <saho.ebrima@ymail.com>
2019-06-30 15:58:27 -0700

    Added server.js with dependencies

---
+++ server.js
@@ -0,0 +1,9 @@
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();

e5eb168dce9bf5ba2ac20036aa0a868868a47317
Ebrima saho <saho.ebrima@ymail.com>
2019-06-30 16:29:47 -0700

    models and public folder added

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 // Dependencies
 var express = require("express");
-var mongojs = require("mongojs");
+// var mongojs = require("mongojs");
+var mongoose = require("mongoose");
+
 // Require axios and cheerio. This makes the scraping possible
 var axios = require("axios");
 var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
 
 // Initialize Express
 var app = express();
